21ef9fa60fd078cca32f9bb3a1aac944d57a0ed1
Pavel <126060406+PavelMakeenkoSPB@users.noreply.github.com>
2023-03-22 17:56:09 +0300

    1

--- Selenium/HW4_World of/steps/world_test.py
+++ Selenium/HW4_World of/steps/world_test.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
-import parse
 
  #================ GENERAL FUNCTIONS ==================#
   
@@ -29,8 +28,8 @@ def step(context, url):
     context.browser.maximize_window()
     context.browser.get(url)
  
- #Проверим, что мы на странице с результатами поиска, есть некоторый искомый текст
-@when("page include text '{text}'")
+#Проверим, что мы на странице с результатами поиска, есть некоторый искомый текст
+@then("page include text '{text}'")
 def step(context, text):
     WebDriverWait(context.browser, 120).until(
         EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "%s")]' % text))
@@ -50,37 +49,34 @@ def step(context, text):
 #Теперь нажмем на кнопку "Поиск в Google"  
 @when("push Google Search button with text '{text}'")
 def step(context, text):
-
     context.browser.find_element(By.CSS_SELECTOR, '[name="btnK"]').click()
    
     
-# #Проверяем, что количество выданных поиском результатов на странице равно 9
-@when("count the number of results per page is 9")
+#Проверяем, что количество выданных поиском результатов на странице равно 9
+@when("number of search results per page is 9")
 def step(context):
+    CounterOfResultOnPage = context.browser.find_elements(By.XPATH, '//*[@id="rso"]/div') 
     
-    counter_array = context.browser.find_elements(By.XPATH, '//*[@id="rso"]/div') 
-    
-    assert int(len(counter_array)) == 9
+    assert int(len(CounterOfResultOnPage)) == 9
     
     
-#Проверяем наличие отчёта о найденных результатах
-@when ("amount of search results exists '{text}'")
-def step(context, text):
-   
-    WebDriverWait(context.browser, 120).until(
-        EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "%s")]' % text))
-    )
-    result_exists = context.browser.find_element(By.XPATH, '//*[@id="result-stats"]').is_displayed()
+#Проверяем наличие отчёта о найденных результатах и его содержание
+@when ("counter of search contains number of results")
+def step(context, ):
+    TextOfResault = context.browser.find_element(By.XPATH, '//*[@id="result-stats"]').is_displayed()
+    context.browser.find_element(By.XPATH,'//*[contains(text(), "Результатов: примерно 777")]')
     
-    assert result_exists == True and context.browser.find_element(By.XPATH,'//*[contains(text(), "%s")]' % text)
-
+       
 #Проверяем правильность размещения картинок в результатах поиска
-@when ("image is displayed in results")
+@when ("all of 4 images are displayed in results correctly")
 def step(context):
     context.browser.implicitly_wait(20)
-    result_image = context.browser.find_element(By.TAG_NAME,'img').is_displayed()
+    ImageInResult = context.browser.find_element(By.XPATH,'//*[@alt = "Бернский зенненхунд, источник: ru.wikipedia.org"]').is_displayed()
+    StyleCorrect = context.browser.find_elements(By.XPATH,"//*[contains(@style,'border-radius:8px;height:92px;width:92px')]")
 
-    assert result_image is True
+    assert ImageInResult == True and int(len(StyleCorrect)) == 4
+    
+
  
  
 # =======================  World of ================================ 
@@ -96,5 +92,7 @@ def step(context):
    s = context.browser.find_element("By.xpath(//div[@class='sbic vYOkbe' and style='background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCKjfMoEu9i-j5v2RNUJcXFQl9UHQ3zhme63IhreRHwmAOkiK8EhaRiQJL&s=10');")
    assert s is True 
 
-    
+@when ('the number of options in the drop-down list')
+def step(context):
+    OptionsInList = context.browser.find_elements("By.XPATH(
     
--- Selenium/HW4_World of/w.feature
+++ Selenium/HW4_World of/w.feature
@@ -1,13 +1,13 @@
+Feature: Проверка результатов поиска "World of tanks" через Google в браузерах Chrome и Edge
 
-Feature: Проверка результатов поиска Google в браузерах Chrome и Edge
-
-Scenario: Поиск запроса Google через Chrome
+Scenario: Поиск запроса "World of tanks" в Google через Chrome
 
   Given on Chrome website "https://www.google.com/"
-  When insert to field text 'world of'
+  When insert to field text "World of tanks"
   When push Google Search button with text 'Поиск в Google'
-  When page include text 'world of'
-  When click into search field
-  When first falling object in field
+  
+  When number of search results per page is 9
+  When all of 4 images are displayed in results correctly
+  Then page include text "World of tanks"
   
   
