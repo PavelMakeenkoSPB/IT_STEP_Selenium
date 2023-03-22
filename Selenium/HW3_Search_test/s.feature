e323c41f9aa48fa0998b11cbc9c9628e8145ca17
Pavel <126060406+PavelMakeenkoSPB@users.noreply.github.com>
2023-03-22 15:12:00 +0300

    HW3_Search_test
    Содержит ДР, где необходимо проверить количество выданных поиском результатов на странице, общее количество найденных страниц и тд.

--- Selenium/HW3_Search_test/s.feature
+++ Selenium/HW3_Search_test/s.feature
@@ -1,24 +1,24 @@
 
-Feature: Проверка результатов поиска Google в браузерах Chrome и Edge
+Feature: Проверка результатов поиска "Бернский зенненхунд" через Google в браузерах Chrome и Edge
 
-Scenario: Поиск запроса Google через Chrome
+Scenario: Поиск запроса 'Бернский зенненхунд' в Google через Chrome
 
   Given on Chrome website "https://www.google.com/"
   When insert to field text 'Бернский зенненхунд'
   When push Google Search button with text 'Поиск в Google'
-  When amount of search results exists 'Результатов: примерно '
-  When count the number of results per page is 9
-  When image is displayed in results
+  When counter of search contains number of results
+  When number of search results per page is 9
+  When all of 4 images are displayed in results correctly
   Then page include text 'Бернский зенненхунд'
   
-Scenario: Поиск запроса Google через Edge
+Scenario: Поиск запроса 'Бернский зенненхунд' в Google через Edge
 
-  Given on Edge website "https://www.google.com/"
+  Given on Chrome website "https://www.google.com/"
   When insert to field text 'Бернский зенненхунд'
   When push Google Search button with text 'Поиск в Google'
-  When amount of search results exists 'Результатов: примерно '
-  When count the number of results per page is 9
-  When image is displayed in results
+  When counter of search contains number of results
+  When number of search results per page is 9
+  When all of 4 images are displayed in results correctly
   Then page include text 'Бернский зенненхунд'
 
 
--- Selenium/HW3_Search_test/steps/search_test.py
+++ Selenium/HW3_Search_test/steps/search_test.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
-import parse
 
  #================ GENERAL FUNCTIONS ==================#
   
@@ -29,7 +28,7 @@ def step(context, url):
     context.browser.maximize_window()
     context.browser.get(url)
  
- #Проверим, что мы на странице с результатами поиска, есть некоторый искомый текст
+#Проверим, что мы на странице с результатами поиска, есть некоторый искомый текст
 @then("page include text '{text}'")
 def step(context, text):
     WebDriverWait(context.browser, 120).until(
@@ -50,38 +49,30 @@ def step(context, text):
 #Теперь нажмем на кнопку "Поиск в Google"  
 @when("push Google Search button with text '{text}'")
 def step(context, text):
-
     context.browser.find_element(By.CSS_SELECTOR, '[name="btnK"]').click()
    
     
-# #Проверяем, что количество выданных поиском результатов на странице равно 9
-@when("count the number of results per page is 9")
+#Проверяем, что количество выданных поиском результатов на странице равно 9
+@when("number of search results per page is 9")
 def step(context):
+    CounterOfResultOnPage = context.browser.find_elements(By.XPATH, '//*[@id="rso"]/div') 
     
-    counter_array = context.browser.find_elements(By.XPATH, '//*[@id="rso"]/div') 
-    
-    assert int(len(counter_array)) == 9
+    assert int(len(CounterOfResultOnPage)) == 9
     
     
-#Проверяем наличие отчёта о найденных результатах
-@when ("amount of search results exists '{text}'")
-def step(context, text):
-   
-    WebDriverWait(context.browser, 120).until(
-        EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "%s")]' % text))
-    )
-    result_exists = context.browser.find_element(By.XPATH, '//*[@id="result-stats"]').is_displayed()
+#Проверяем наличие отчёта о найденных результатах и его содержание
+@when ("counter of search contains number of results")
+def step(context, ):
+    TextOfResault = context.browser.find_element(By.XPATH, '//*[@id="result-stats"]').is_displayed()
+    context.browser.find_element(By.XPATH,'//*[contains(text(), "Результатов: примерно 777")]')
     
-    assert result_exists == True and context.browser.find_element(By.XPATH,'//*[contains(text(), "%s")]' % text)
-
+       
 #Проверяем правильность размещения картинок в результатах поиска
-@when ("image is displayed in results")
+@when ("all of 4 images are displayed in results correctly")
 def step(context):
     context.browser.implicitly_wait(20)
-    result_image = context.browser.find_element(By.TAG_NAME,'img').is_displayed()
+    ImageInResult = context.browser.find_element(By.XPATH,'//*[@alt = "Бернский зенненхунд, источник: ru.wikipedia.org"]').is_displayed()
+    StyleCorrect = context.browser.find_elements(By.XPATH,"//*[contains(@style,'border-radius:8px;height:92px;width:92px')]")
 
-    assert result_image is True
-    
-
-
+    assert ImageInResult == True and int(len(StyleCorrect)) == 4
     
